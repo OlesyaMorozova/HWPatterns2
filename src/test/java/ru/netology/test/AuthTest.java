b8decd4f7cee0d59864642089233c59ccf1a382c
Olesya Morozova <ovmorozova12@yandex.ru>
2024-09-11 23:35:51 +0400

    Initial commit

---
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -0,0 +1,30 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.data.DataGenerator.Registration.getUser;
+import static ru.netology.data.DataGenerator.getRandomLogin;
+import static ru.netology.data.DataGenerator.getRandomPassword;
+
+
+public class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void registeredUserLogin() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
+    }
+}
